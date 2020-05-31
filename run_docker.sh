5591118dbeca5ccbc0be6fc4b6e3b2d8ed0583b1
Lars Maertins <maertins@changes.ngo>
2020-06-01 01:28:03 +0200

    Only listing docker images from udcity-prj4-maertins repo

--- run_docker.sh
+++ run_docker.sh
@@ -9,12 +9,12 @@ docker rm ucity-prj4
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t udacity-prj4-maertins:1.1 .
+docker build -t udacity-prj4-maertins:1.0 .
 
 # Step 2: 
 # List docker images
-docker images --all
+docker images udacity-prj4-maertins
 
 # Step 3: 
 # Run flask app
-docker run --publish 8000:80 --name ucity-prj4 udacity-prj4-maertins:1.1
+docker run --publish 8000:80 --name ucity-prj4 udacity-prj4-maertins:1.0
